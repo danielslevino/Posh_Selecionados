e79301e99e8c270bdf8f66ea4babf6157ccde47b
danielslevino <128863623+danielslevino@users.noreply.github.com>
2023-04-21 11:37:29 -0300

    Add files via upload
    Obter informações de todas as  placa de rede instaladas no computador.

---
+++ Computer_Info-Mac_Adress.ps1
@@ -0,0 +1,40 @@
+﻿#Obter o nome do computador
+$computerName = $env:COMPUTERNAME
+
+#Obter informações de todas as placas de rede instaladas no computador
+$netAdapters = Get-WmiObject Win32_NetworkAdapter
+
+#Criar um objeto para armazenar as informações
+$output = @()
+
+#Loop por cada placa de rede
+foreach ($adapter in $netAdapters) {
+    #Obter o nome da placa de rede e o endereço físico
+    $name = $adapter.Name
+    $macAddress = $adapter.MACAddress
+
+    #Criar um objeto com as informações
+    $netAdapterInfo = [PSCustomObject]@{
+        'Nome do computador' = $computerName
+        'Nome da placa de rede' = $name
+        'Endereço físico' = $macAddress
+    }
+
+    #Adicionar o objeto ao array de saída
+    $output += $netAdapterInfo
+}
+
+#Criar o caminho para o arquivo
+$outputPath = "\\DANIEL-PC\Users\Daniel & Danielle\Desktop\TesTe\Computer_Info"
+
+#Verificar se o arquivo .txt já existe, se sim, adiciona a informação no final, se não, cria um novo arquivo
+if(Test-Path "$outputPath.txt"){
+    $output | Export-Csv "$outputPath.csv" -Append -NoTypeInformation
+    $output | Out-File "$outputPath.txt" -Append
+} else {
+    $output | Export-Csv "$outputPath.csv" -NoTypeInformation
+    $output | Out-File "$outputPath.txt"
+}
+
+#Exibir as informações na tela
+$output | Format-Table
